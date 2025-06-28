18f35ddb6c2285dae57c0506023f17856e5bc650
benjamin <benjaminurrea14@gmail.com>
2025-06-27 20:34:42 -0400

    Avance

--- scr/src/main/java/Vista/VentanaTrabajador.java
+++ scr/src/main/java/Vista/VentanaTrabajador.java
@@ -1,4 +1,21 @@
-package Vista;
+package vista;
 
-public class VentanaTrabajador {
+import modelo.*;
+import javax.swing.*;
+
+public class VentanaTrabajador extends JFrame {
+    private GestorTrabajador gestor = new GestorTrabajador();
+
+    public VentanaTrabajador() {
+        btnGuardar.addActionListener(e -> {
+            Trabajador t = new Trabajador(
+                    txtNombre.getText(),
+                    "",
+                    txtRut.getText(),
+                    (Isapre) cbIsapre.getSelectedItem(),
+                    (AFP) cbAfp.getSelectedItem()
+            );
+            gestor.guardarTrabajador(t);
+        });
+    }
 }
